fc17b9b9afa8247cc62df54f1a564c48e5ecfbfe
Olumide Adeniyi <43913998+oadeniyi23@users.noreply.github.com>
2022-07-25 00:20:54 +0300

    Update bigquery_scenario_2_step_3_create_fact_table_daily_trips.py

--- chapter-3/code/bigquery_scenario_2_step_3_create_fact_table_daily_trips.py
+++ chapter-3/code/bigquery_scenario_2_step_3_create_fact_table_daily_trips.py
@@ -15,16 +15,16 @@ def create_fact_table(PROJECT_ID, TARGET_TABLE_ID):
     write_disposition='WRITE_APPEND')
 
     sql = """SELECT DATE(start_date) as trip_date,
-          start_station_id,
-          COUNT(trip_id) as total_trips,
-          SUM(duration_sec) as sum_duration_sec,
-          AVG(duration_sec) as avg_duration_sec
-          FROM `{PROJECT_ID}.raw_bikesharing.trips` trips
-          JOIN `{load_date}.raw_bikesharing.stations` stations
-          ON trips.start_station_id = stations.station_id
-          WhERE DATE(start_date) = DATE('{}')
-          GROUP BY trip_date, start_station_id
-          ;""".format(PROJECT_ID=PROJECT_ID, load_date=load_date)
+                 start_station_id,
+                 COUNT(trip_id) as total_trips,
+                 SUM(duration_sec) as sum_duration_sec,
+                 AVG(duration_sec) as avg_duration_sec
+                 FROM `{PROJECT_ID}.raw_bikesharing.trips` trips
+                 JOIN `{PROJECT_ID}.raw_bikesharing.stations` stations
+                 ON trips.start_station_id = stations.station_id
+                 WhERE DATE(start_date) = DATE('{load_date}')
+                 GROUP BY trip_date, start_station_id
+                 ;""".format(PROJECT_ID=PROJECT_ID, load_date=load_date)
 
     query_job = client.query(sql, job_config=job_config)
 
